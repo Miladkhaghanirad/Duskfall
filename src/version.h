733b82408fd2e2d994fc1bb2618d4a8412b4daf8
Raine 'Gravecat' Simmons <cat@moxx.net>
2019-11-02 08:29:38 +0000

    Added a note to contributors on version.h

--- src/version.h
+++ src/version.h
@@ -4,6 +4,11 @@
 #pragma once
 #include "duskfall.h"
 
+// NOTE TO CONTRIBUTORS!
+// Please DO NOT adjust the version numbers here! I already have a versioning system in mind for the official releases of Duskfall, and things would great all mixed up
+// and confusing if indivudual modders/developers started changing the version numbers in their pull requests. Please leave these numbers as-is, and I will assign new
+// version numbers personally to all code that is added to the master branch.
+
 #define DUSKFALL_VERSION_EPOCH		0
 #define DUSKFALL_VERSION_MAJOR		9
 #define DUSKFALL_VERSION_MINOR		2
