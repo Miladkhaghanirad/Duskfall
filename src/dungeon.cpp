Miladkhaghanirad/Duskfall

commit f98b1ff85c9500c82731e4ec52d9ae83cce47c07

Removed the static-data hash lookup crap (#60), removed the defunct sidebar code.

src/dungeon.cpp
@@ -510,7 +510,7 @@ void Dungeon::random_start_position(unsigned short &x, unsigned short &y)
 	{
 		x = mathx::rnd(width - 4) + 2;
 		y = mathx::rnd(height - 4) + 2;
-		if (tiles[x + y * width].name == strx::hash("floor")) return;
+		if (tiles[x + y * width].floor()) return;
 	}
 }
 
@@ -703,7 +703,7 @@ bool Dungeon::viable_doorway(unsigned short x, unsigned short y)
 {
 	STACK_TRACE();
 	if (x < 2 || y < 2 || x >= width - 2 || y >= height - 2) return false;
-	if (tiles[x + y * width].name != strx::hash("floor")) return false;	// Only basic floor can become a door.
+	if (!tiles[x + y * width].floor()) return false;	// Only basic floor can become a door.
 
 	if (tiles[x - 1 + y * width].wall() && tiles[x + 1 + y * width].wall())
 	{
@@ -773,14 +773,15 @@ string Tile::check_neighbours(int x, int y, bool wall)
 string Tile::get_name()
 {
 	STACK_TRACE();
-	return data::tile_name(name);
+	string output = name;
+	return output;
 }
 
 // Returns the sprite name for rendering this tile.
 string Tile::get_sprite(int x, int y)
 {
 	STACK_TRACE();
-	string sprite_name = data::tile_sprite(sprite);
+	string sprite_name = sprite;
 
 	if (sprite_name.size() >= 7 && sprite_name.substr(0, 6) == "FLOOR_") return sprite_name.substr(0, 7) + "_" + check_neighbours(x, y, false);
 	else if (sprite_name.size() >= 6 && sprite_name.substr(0, 5) == "WALL_") return sprite_name.substr(0, 6) + "_" + check_neighbours(x, y, true);
@@ -795,4 +796,30 @@ bool Tile::neighbour_identical(int x, int y)
 	shared_ptr<Tile> neighbour = world::dungeon()->tile(x, y);
 	if (neighbour->sprite == sprite) return true;
 	else return false;
+}
+
+// Sets the name of this Tile.
+void Tile::set_name(string new_name)
+{
+	STACK_TRACE();
+	if (new_name.size() >= TILE_NAME_MAX)
+	{
+		guru::log("Tile name too long: " + new_name, GURU_ERROR);
+		new_name = new_name.substr(0, TILE_NAME_MAX - 1);
+	}
+	memset(&name[0], 0, TILE_NAME_MAX);
+	std::copy(new_name.begin(), new_name.end(), &name[0]);
+}
+
+// Sets the sprite for this Tile.
+void Tile::set_sprite(string new_sprite)
+{
+	STACK_TRACE();
+	if (new_sprite.size() >= TILE_SPRITE_MAX)
+	{
+		guru::log("Tile sprite name too long: " + new_sprite, GURU_ERROR);
+		new_sprite = new_sprite.substr(0, TILE_SPRITE_MAX - 1);
+	}
+	memset(&sprite[0], 0, TILE_SPRITE_MAX);
+	std::copy(new_sprite.begin(), new_sprite.end(), &sprite[0]);
 }

src/version.h
@@ -12,7 +12,7 @@
 #define DUSKFALL_VERSION_EPOCH		0
 #define DUSKFALL_VERSION_MAJOR		17
 #define DUSKFALL_VERSION_MINOR		5
-#define DUSKFALL_VERSION_PATCH		0
+#define DUSKFALL_VERSION_PATCH		1
 #define DUSKFALL_EDITION			"A New Beginning"
 
 #define DUSKFALL_MAJOR_VERSION_STRING	(std::to_string(DUSKFALL_VERSION_EPOCH) + "." + std::to_string(DUSKFALL_VERSION_MAJOR) + "." + std::to_string(DUSKFALL_VERSION_MINOR))

src/controls.cpp
@@ -6,7 +6,6 @@
 #include "hero.h"
 #include "iocore.h"
 #include "message.h"
-#include "sidebar.h"
 #include "static-data.h"
 #include "world.h"
 

src/dungeon.h
@@ -14,27 +14,34 @@ class Actor;	// defined in actor.h
 #define TILE_FLAG_EXPLORED		(1 << 4)
 #define TILE_FLAG_FLOOR			(1 << 5)
 
+#define TILE_NAME_MAX	16	// The longest possible name for a Tile.
+#define TILE_SPRITE_MAX	16	// The longest possible sprite name for a Tile.
+
 
 class Tile
 {
 public:
-	unsigned char	flags;	// Various properties of this tile.
-	unsigned int	name;	// An integer for the tile's hashed name, which is decoded elsewhere.
-	unsigned int	sprite;	// An integer for the tile's hashed tile sprite, which is decoded elsewhere.
-
-			Tile() : flags(0), name(0), sprite(0) { }
+			Tile() : flags(0) { }
 	bool	destroyable_wall() { return wall() && !permawall(); }
 	bool	explored() { return (flags & TILE_FLAG_EXPLORED) == TILE_FLAG_EXPLORED; }
+	bool	floor() { return (flags & TILE_FLAG_FLOOR) == TILE_FLAG_FLOOR; }
 	string	get_name();		// Returns the name of this tile.
 	string	get_sprite(int x, int y);	// Returns the sprite name for rendering this tile.
 	bool	impassible() { return (flags & TILE_FLAG_IMPASSIBLE) == TILE_FLAG_IMPASSIBLE; }
 	bool	opaque() { return (flags & TILE_FLAG_OPAQUE) == TILE_FLAG_OPAQUE; }
 	bool	permawall() { return (flags & TILE_FLAG_PERMAWALL) == TILE_FLAG_PERMAWALL; }
+	void	set_name(string new_name);		// Sets the name of this Tile.
+	void	set_sprite(string new_sprite);	// Sets the sprite for this Tile.
 	bool	wall() { return (flags & TILE_FLAG_WALL) == TILE_FLAG_WALL; }
+
+	unsigned char	flags;		// Various properties of this tile.
 
 private:
 	string	check_neighbours(int x, int y, bool wall);	// Checks nearby tiles to modify floor and wall sprites.
 	bool	neighbour_identical(int x, int y);	// Check if a neighbour is an identical tile.
+
+	char	name[TILE_NAME_MAX];		// An integer for the tile's hashed name, which is decoded elsewhere.
+	char	sprite[TILE_SPRITE_MAX];	// An integer for the tile's hashed tile sprite, which is decoded elsewhere.
 };
 
 class Dungeon

src/hero.cpp
@@ -8,7 +8,6 @@
 #include "iocore.h"
 #include "hero.h"
 #include "message.h"
-#include "sidebar.h"
 #include "strx.h"
 #include "world.h"
 

src/message.cpp
@@ -6,7 +6,6 @@
 #include "iocore.h"
 #include "message.h"
 #include "prefs.h"
-#include "sidebar.h"
 #include "strx.h"
 #include "world.h"
 
@@ -64,7 +63,7 @@ void msg(string message, MC colours)
 {
 	STACK_TRACE();
 	bool redraw = true;
-	iocore::rect(iocore::get_cols() - SIDEBAR_WIDTH_8X8 - 1, iocore::get_rows() - 1, SIDEBAR_WIDTH_8X8 + 1, 1, Colour::BLACK);
+	iocore::rect(iocore::get_cols() - 6, iocore::get_rows() - 1, 6, 1, Colour::BLACK);
 	unsigned int key = 0;
 	while (++messages_since_last_reset > MESSAGE_LOG_SIZE)
 	{
@@ -73,8 +72,7 @@ void msg(string message, MC colours)
 			iocore::cls();
 			world::dungeon()->render();
 			message::render();
-			sidebar::render();
-			iocore::print("-more-", iocore::get_cols_narrow() - SIDEBAR_WIDTH_5X8, iocore::get_rows() - 1, Colour::CGA_WHITE, PRINT_FLAG_NARROW);
+			iocore::print("-more-", iocore::get_cols_narrow(), iocore::get_rows() - 1, Colour::CGA_WHITE, PRINT_FLAG_NARROW);
 			iocore::flip();
 			redraw = false;
 		}
@@ -182,7 +180,7 @@ void purge_buffer()
 void render()
 {
 	STACK_TRACE();
-	iocore::rect(0, iocore::get_rows() - MESSAGE_LOG_SIZE, iocore::get_cols() - SIDEBAR_WIDTH_8X8 - 1, MESSAGE_LOG_SIZE, Colour::BLACK);
+	iocore::rect(0, iocore::get_rows() - MESSAGE_LOG_SIZE, iocore::get_cols() - 1, MESSAGE_LOG_SIZE, Colour::BLACK);
 	if (output_prc.size())
 	{
 		unsigned int end = output_prc.size();

src/sidebar.cpp
@@ -1,130 +1,0 @@
-
-// sidebar.cpp -- The sidebar, displaying information about the nearby elements of the dungeon as well as player stats.
-// Copyright (c) 2019 Raine "Gravecat" Simmons. All rights reserved.
-
-#include "actor.h"
-#include "dungeon.h"
-#include "iocore.h"
-#include "sidebar.h"
-#include "static-data.h"
-
-#include "guru.h"
-
-
-namespace sidebar
-{
-
-vector<shared_ptr<Actor>>	actors_in_sight;	// The list of visible Actors.
-unsigned int				sidebar_row = 1;	// The current row we're writing on.
-vector<shared_ptr<Tile>>	tiles_in_sight;		// The list of visible Tiles.
-
-
-// Tells the sidebar system that an Actor is visible.
-void actor_in_sight(shared_ptr<Actor> actor)
-{
-	STACK_TRACE();
-	for (auto actor_check : actors_in_sight)
-		if (actor_check->name == actor->name) return;
-	for (unsigned int i = 0; i < actors_in_sight.size(); i++)
-	{
-		if (actor->name < actors_in_sight.at(i)->name)
-		{
-			actors_in_sight.insert(actors_in_sight.begin() + i, actor);
-			return;
-		}
-	}
-	actors_in_sight.push_back(actor);
-}
-
-// Writes a line of text on the sidebar.
-void print(string text, unsigned int offset_x)
-{
-	STACK_TRACE();
-	unsigned int x_pos = iocore::get_cols_narrow() - SIDEBAR_WIDTH_5X8 + offset_x;
-	iocore::ansi_print(text, x_pos + offset_x, sidebar_row, PRINT_FLAG_NARROW);
-	sidebar_row++;
-}
-
-// Renders the sidebar!
-void render()
-{
-	STACK_TRACE();
-	/*
-	iocore::rect(iocore::get_cols() - SIDEBAR_WIDTH_8X8 - 1, 0, SIDEBAR_WIDTH_8X8 + 1, iocore::get_rows() - 1, Colour::BLACK);
-	sidebar_row = 1;
-
-	if (actors_in_sight.size())
-	{
-		bool monsters = false, items = false;
-		for (auto actor : actors_in_sight)
-		{
-			if (actor->is_monster()) monsters = true;
-			if (actor->is_item()) items = true;
-			if (monsters && items) break;
-		}
-
-		if (monsters)
-		{
-			sidebar::print("{5F}Monsters nearby:");
-			for (auto actor : actors_in_sight)
-			{
-				if (!actor->is_monster()) continue;
-				iocore::print_at(actor->glyph, iocore::get_cols() - SIDEBAR_WIDTH_8X8 + 1, sidebar_row, actor->colour);
-				sidebar::print("{57}" + actor->name, 1);
-			}
-			sidebar_row++;
-		}
-
-		if (items)
-		{
-			sidebar::print("{5F}Items nearby:");
-			for (auto actor : actors_in_sight)
-			{
-				if (!actor->is_item()) continue;
-				iocore::print_at(actor->glyph, iocore::get_cols() - SIDEBAR_WIDTH_8X8 + 1, sidebar_row, actor->colour);
-				sidebar::print("{57}" + actor->name, 1);
-			}
-			sidebar_row++;
-		}
-	}
-
-	if (tiles_in_sight.size())
-	{
-		sidebar::print("{5F}Features:");
-		for (auto tile : tiles_in_sight)
-		{
-			iocore::print_at(static_cast<Glyph>(tile->glyph), iocore::get_cols() - SIDEBAR_WIDTH_8X8 + 1, sidebar_row, tile->colour);
-			sidebar::print("{57}" + data::tile_name(tile->name), 1);
-		}
-		sidebar_row++;
-	}
-	*/
-}
-
-// Tells the sidebar system to forget the Actors and Tiles it has stored.
-void reset_lists()
-{
-	STACK_TRACE();
-	actors_in_sight.clear();
-	tiles_in_sight.clear();
-}
-
-// Tells the sidebar system that a Tile is visible.
-void tile_in_sight(Tile tile)
-{
-	STACK_TRACE();
-	for (auto tile_check : tiles_in_sight)
-		if (tile_check->name == tile.name) return;
-	shared_ptr<Tile> tile_ptr = std::make_shared<Tile>(tile);
-	for (unsigned int i = 0; i < tiles_in_sight.size(); i++)
-	{
-		if (data::tile_name(tile.name) < data::tile_name(tiles_in_sight.at(i)->name))
-		{
-			tiles_in_sight.insert(tiles_in_sight.begin() + i, tile_ptr);
-			return;
-		}
-	}
-	tiles_in_sight.push_back(tile_ptr);
-}
-
-}	// namespace sidebar

src/sidebar.h
@@ -1,23 +1,0 @@
-// sidebar.h -- The sidebar, displaying information about the nearby elements of the dungeon as well as player stats.
-// Copyright (c) 2019 Raine "Gravecat" Simmons. All rights reserved.
-
-#pragma once
-#include "duskfall.h"
-
-class Actor;	// defined in actor.h
-class Tile;		// defined in dungeon.h
-
-#define SIDEBAR_WIDTH_8X8	16
-#define SIDEBAR_WIDTH_5X8	24
-
-
-namespace sidebar
-{
-
-void	actor_in_sight(shared_ptr<Actor> actor);	// Tells the sidebar system that an Actor is visible.
-void	print(string text, unsigned int offset_x = 0);	// Writes a line of text on the sidebar.
-void	render();					// Renders the sidebar!
-void	reset_lists();				// Tells the sidebar system to forget the Actors and Tiles it has stored.
-void	tile_in_sight(Tile tile);	// Tells the sidebar system that a Tile is visible.
-
-}	// namespace sidebar

commit 656194ee403193435337e0afcaf464e33940b286

Fixed a bug after commit f98b1ff which caused walls to no longer tile properly.

src/dungeon.cpp
@@ -773,8 +773,7 @@ string Tile::check_neighbours(int x, int y, bool wall)
 string Tile::get_name()
 {
 	STACK_TRACE();
-	string output = name;
-	return output;
+	return string(name);
 }
 
 // Returns the sprite name for rendering this tile.
@@ -794,7 +793,7 @@ bool Tile::neighbour_identical(int x, int y)
 	STACK_TRACE();
 	if (x < 0 || y < 0 || x >= world::dungeon()->get_width() || y >= world::dungeon()->get_height()) return false;
 	shared_ptr<Tile> neighbour = world::dungeon()->tile(x, y);
-	if (neighbour->sprite == sprite) return true;
+	if (string(neighbour->sprite) == string(sprite)) return true;
 	else return false;
 }
 

src/version.h
@@ -12,7 +12,7 @@
 #define DUSKFALL_VERSION_EPOCH		0
 #define DUSKFALL_VERSION_MAJOR		17
 #define DUSKFALL_VERSION_MINOR		5
-#define DUSKFALL_VERSION_PATCH		2
+#define DUSKFALL_VERSION_PATCH		3
 #define DUSKFALL_EDITION			"A New Beginning"
 
 #define DUSKFALL_MAJOR_VERSION_STRING	(std::to_string(DUSKFALL_VERSION_EPOCH) + "." + std::to_string(DUSKFALL_VERSION_MAJOR) + "." + std::to_string(DUSKFALL_VERSION_MINOR))